4a129a6a1efc6faea56f698344bed565f025f27b
AnkushSJ <134841188+AnkushSJ@users.noreply.github.com>
2026-06-20 22:48:53 +0530

    Trees

---
+++ Trees/Traversals/RightView.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    void solve(TreeNode* root , vector<int> &ans , int level)
+    {
+        if(root==NULL)
+            return;
+        
+        if(level == ans.size())
+            ans.push_back(root->val);
+
+        solve(root->right,ans,level+1);
+        solve(root->left,ans,level+1);
+    }
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        solve(root,ans,0);
+        return ans;
+    }
+};
