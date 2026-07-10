ae9a1b9264636ca07a2cef55bec713da8befca4a
AnkushSJ <134841188+AnkushSJ@users.noreply.github.com>
2026-07-10 09:00:59 +0530

    DP

--- Dynamic_Programming/Fibonacci _Number.cpp
+++ Dynamic_Programming/Fibonacci _Number.cpp
@@ -10,3 +10,55 @@ int fib(int n) {
     return fibonacci(n);
 }
 
+// Top-Down Approach(Memorization)
+int fibonacci(int n, vector<int>&dp){ 
+    if(n<=1){
+        return n;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    return dp[n] = fibonacci(n-1,dp)+fibonacci(n-2,dp);
+}
+
+int fib(int n) {
+    vector<int>dp(n+1,-1);
+    return fibonacci(n,dp);
+}
+
+// Bottom-Up Approach(Tabulation)
+int fib(int n) {
+    if(n<=1)
+    return n;
+
+    vector<int>dp(n+1);
+    // n = 0, dp = [0]
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+}
+
+// Space Optimization
+int fib(int n) {
+    if(n<=1)
+    return n;
+
+    if(n==2)
+    return 1;
+
+    vector<int>dp(3); // can use const variables instead of array 
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for(int i=3;i<=n;i++){
+        dp[0] = dp[1];
+        dp[1] = dp[2];
+        dp[2] = dp[0]+dp[1];
+    }
+
+    return dp[2];
+}
